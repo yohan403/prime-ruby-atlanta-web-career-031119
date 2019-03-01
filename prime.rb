302b96b7a8ef18c92ccf8991678024e7cadefc08
John Cho <johnycho16@gmail.com>
2019-03-01 22:38:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 # Add  code here!
 def prime?(int)
   prime_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
-  sq_rt = Math.sqrt(int)
   boolean_list = []
   if int > 0
+    sq_rt = Math.sqrt(int)
     for i in prime_list do
       while i < sq_rt do
         remainder = int % i
