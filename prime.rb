92799e087235026b3711a39dea2dc46843dc11eb
John Cho <johnycho16@gmail.com>
2019-03-01 22:36:30 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,24 +3,24 @@ def prime?(int)
   prime_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
   sq_rt = Math.sqrt(int)
   boolean_list = []
-  if int < 0
-    return FALSE
-    break
-  end
-  for i in prime_list do
-    while i < sq_rt do
-      remainder = int % i
-      if remainder == 0
-        boolean_list.push(FALSE)
-      else
-        boolean_list.push(TRUE)
+  if int > 0
+    for i in prime_list do
+      while i < sq_rt do
+        remainder = int % i
+        if remainder == 0
+          boolean_list.push(FALSE)
+        else
+         boolean_list.push(TRUE)
+       end
+        break
       end
-      break
     end
-  end
-  if boolean_list.include?(FALSE)
-    return FALSE
+    if boolean_list.include?(FALSE)
+      return FALSE
+    else
+      return TRUE
+    end
   else
-    return TRUE
+    return FALSE
   end
 end
