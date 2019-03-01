1958228c524a733e4edddff55cfd72a435dc544f
John Cho <johnycho16@gmail.com>
2019-03-01 22:11:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,6 @@
 # Add  code here!
 def prime?(int)
   prime_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
-  
+  square_rt = sqrt(int)
+  while 
 end
