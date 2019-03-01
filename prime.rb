f0dcc37125093b6d91d2d4f1493f0ed757fc8ef5
John Cho <johnycho16@gmail.com>
2019-03-01 22:17:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,8 @@
 # Add  code here!
 def prime?(int)
   prime_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
-  
+  sq_rt = Math.sqrt(int)
+  for i in prime_list do
+    if sq_rt < i
       
 end
